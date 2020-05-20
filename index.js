0837247552e3cc1ba30ab8e444811a519a0fdca2
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-20 22:17:37 +0200

    added styling and refactored

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const password2 = document.getElementById("password_2");
 
 function isValidEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(email.value)) {
+    successMessage(email);
+  } else {
+    errorMessage(email, "Email is not valid");
+  }
 }
 
 function errorMessage(input, message) {
@@ -36,9 +40,39 @@ function getFieldName(input) {
   return input.id.substr(0, 1).toUpperCase() + input.id.substr(1);
 }
 
+//check length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    errorMessage(
+      input,
+      `${getFieldName(input)} cannot be less than ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    errorMessage(
+      input,
+      `${getFieldName(input)} cannot be greater than ${max} characters`
+    );
+  } else {
+    successMessage(input);
+  }
+}
+
+//check passwords
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    errorMessage(input2, "Passwords do not match");
+  } else if (input1.value !== input2.value && input1.value !== "") {
+    successMessage(input2);
+  }
+}
+
 //event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkRequired([username, email, password, password_2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  isValidEmail(email);
+  checkPasswordMatch(password, password_2);
 });
